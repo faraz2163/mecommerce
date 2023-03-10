8589f06c81a5c8d419745cf6c925ca46de0247e3
abdul <ahad.ali321@gmail.com>
2023-03-10 21:28:27 +0500

    Started home page front category dynamic

--- src/custom-assets/app.js
+++ src/custom-assets/app.js
@@ -1,6 +1,8 @@
+let categories;
 fetch("https://api.escuelajs.co/api/v1/categories?limit=10")
   .then((res) => res.json())
   .then((categories) => {
+    categories = categories
     const template = document
       .querySelector("#NavigationCategoryListing")
       .content.querySelector("li");
@@ -11,5 +13,23 @@ fetch("https://api.escuelajs.co/api/v1/categories?limit=10")
       anchor.querySelector("span").innerHTML = category.name;
       anchor.querySelector("img").src = category.image;
       document.getElementById("NavCatgegoriesUL").append(tempTemplate);
+    });
+  });
+
+  let products;
+fetch("https://api.escuelajs.co/api/v1/categories?limit=10")
+  .then((response) => response.json()).then((products) => {
+    products = products
+    const temp = document.querySelector("#left-banner-products").content.querySelector(".col-md-6");
+    products.forEach((product) => {
+      const protemp = temp.cloneNode(true);
+      const img = protemp.querySelector(".collection-img img");
+      const heading3 = protemp.querySelector(".collection-banner-contain div h3");
+      const heading4 = protemp.querySelector(".collection-banner-contain div h4");
+      const aTag = protemp.querySelector(".collection-banner-contain div .btn-rounded");
+      img.src = product.image;
+      document.getElementById("inner-product").append(protemp);
+      // let three = document.getElementById("inner-product").append(protemp);
+      // three.slice(0,3);
     });
   });
