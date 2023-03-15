e2303a94cc63dfeed917669bbe1bda649ae965c9
zaidaftab66 <zaidaftab66@gmail.com>
2023-03-15 19:37:44 +0500

    Hero categories dynamic done

--- src/custom-assets/app.js
+++ src/custom-assets/app.js
@@ -11,5 +11,18 @@ fetch("https://api.escuelajs.co/api/v1/categories?limit=10")
       anchor.querySelector("span").innerHTML = category.name;
       anchor.querySelector("img").src = category.image;
       document.getElementById("NavCatgegoriesUL").append(tempTemplate);
+    });        
+  });
+
+
+fetch("https://api.escuelajs.co/api/v1/categories?limit=3")
+  .then((response) => response.json()).then((products) => {
+    const temp = document.querySelector("#BannerCollection").content.querySelector(".col-xl-12");
+    products.forEach((product) => {
+      const dyncollection = temp.cloneNode(true);
+      dyncollection.querySelector("img").src = product.image;
+      dyncollection.querySelector("h3").innerHTML = product.name;
+      document.getElementById("DynBannerCollection").append(dyncollection);
     });
   });
+  
